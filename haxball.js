f71d15e8de8612c7d75714df7bef1f4e5bc8e45b
Filip Zyskowski <filipzyskowski@gmail.com>
2023-08-25 13:11:00 +0200

    merge scripts into one file

--- haxball.js
+++ haxball.js
@@ -1,5 +1,3 @@
-import PlayerMessageHandler from "./auto-p";
-
 var room = HBInit({
 	roomName: "RU Hax",
 	maxPlayers: 16,
@@ -9,6 +7,50 @@ var room = HBInit({
 room.setDefaultStadium("Big");
 room.setScoreLimit(5);
 room.setTimeLimit(5);
+
+class PlayerMessageHandler {
+    constructor(room) {
+        this.set = new Set();
+        this.room = room;
+    }
+
+    handle(player, message) {
+        switch (message) {
+            case 'p':
+                this.#TryToPause(player);
+                break;
+            case 'unp':
+                this.#TryToUnpause(player);
+            default:
+                break;
+        }
+    }
+
+    #TryToPause(player) {
+        if (this.set.has(player)) {
+            this.room.sendAnnouncement(player.name + ' już poprosił o przerwę.');
+            return;
+        }
+
+        this.room.pauseGame(true);
+        this.room.sendAnnouncement(player.name + ' prosi o przerwę.');
+    }
+
+    #TryToUnpause(player) {
+        if (!this.set.has(player)) {
+            this.room.sendAnnouncement(player.name + ': nie możesz odpauzować trwającej gry.');
+            return;
+        }
+        
+        this.set.delete(player);
+
+        this.room.sendAnnouncement(player.name + ' wrócił z przerwy.');
+
+        if (this.set.size === 0) {
+            this.room.pauseGame(false);
+        }
+    }
+}
 
 const highPlayerCount = 6;
 const hugePlayerCount = 8;
